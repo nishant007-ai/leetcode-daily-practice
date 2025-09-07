9b745de05615a8f584a548d27987bda8c2cc208c
Nishant Jadhav <nnjadhav2004@gmail.com>
2025-09-07 21:34:43 +0530

    Added README with Happy Number + LeetCode solutions

---
+++ day35/real life example /happy number .py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n):
+        def get_next(num):
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit * digit
+                num //= 10
+            return total
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        return n == 1
+
+ #Input: n = 2
+#Output: false
