57618b96e8a2415aaf12ae1275c3414160777479
Nishant Jadhav <nnjadhav2004@gmail.com>
2025-10-29 04:55:07 +0530

    small code update

--- day46/real life example/linked list.py
+++ day46/real life example/linked list.py
@@ -1,28 +1,45 @@
-class listnote (slef,x):
-    self.val=x
-    self.next =None
-
-class SOluti9on:
-    def hash cyclyre (self ,head):
-       slow = head
-    fast = head
-    while slow,fast and fast.next:
-       slow = slow.next
-       fast = fast.next .next
-
-       if slow ==fast :
-           return true 
-     else :
-        return false
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
-    def createlinkedlsit(val,poes):
-        head= listnote (val[0])
-        current = head
-        cycle_node = None           
+class Solution:
+    def hasCycle(self, head):
+        slow = head
+        fast = head
 
-        if index in range (1,len(val)):
-            current.next = listnote (val[index])
-            current = current.next
-            if index == pos:
-                cycle_node = current      
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+
+# Helper function to create a linked list with a cycle
+def createLinkedList(values, pos):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+    cycle_node = None
+
+    for i in range(1, len(values)):
+        current.next = ListNode(values[i])
+        current = current.next
+        if i == pos:
+            cycle_node = current
+
+    if pos != -1:
+        current.next = cycle_node
+
+    return head
+
+
+# 🔍 Test Cases
+
