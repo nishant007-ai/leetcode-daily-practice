074df42c51f3b8e804de1af22745ee8118c2eee3
Nishant Jadhav <nnjadhav2004@gmail.com>
2025-10-30 07:01:51 +0530

    small code update

--- day47/real life example/longconsictive.py
+++ day47/real life example/longconsictive.py
@@ -18,3 +18,10 @@ class Solution:
                 longest_streak = max(longest_streak, current_streak)
 
         return longest_streak
+
+#Input: nums = [100,4,200,1,3,2]
+Output: 4
+
+
+#nput: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
