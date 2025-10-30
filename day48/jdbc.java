f9f5b7b914fae7a8120a74736b5ba5e771902ee0
Nishant Jadhav <nnjadhav2004@gmail.com>
2025-10-30 20:10:28 +0530

    small code update

---
+++ day48/jdbc.java
@@ -0,0 +1,14 @@
+import java.mysql.*;
+      public class jdbc;
+      {
+        public voide main (string[]args){
+            string url = "jdbc:mysql//localhost:3306/dbnames";
+            string user = "root";
+            string password = "root";
+            try {
+               connection con =drivemangaer.getco 
+            } catch (Exception e) {
+            }
+        }
+      }
+print.syste
